bb8f6e24f1f135a2308364ce453d8d616e97d4be
cet-t <cets021@gmail.com>
2024-07-14 12:31:49 +0900

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 ﻿import asyncio
-from collections import UserDict
 from typing import Optional
 import discord
 from discord import app_commands
@@ -11,39 +10,25 @@ from random import randint
 
 from data.member_data import *
 from data.heist_data import *
-from pyenv import channel_ids, file_path, user_ids
-from format2 import LogDict, LogDataDict, LogDataKey, LogKey
+# from pyenv import channel_ids, file_path, user_ids
+from parameter import Parameter, LogDict, LogDataDict
 import utility
 
 bot = discord.Client(intents=discord.Intents.all())
 tree = app_commands.CommandTree(bot)
 
 
-class GetPath:
-    MEMBERS_DIR = '../members'
-    LOG = '../log/cost.json'
-
-    @staticmethod
-    def members(id: int):
-        return f'../members/{id}.json'
-
-    @staticmethod
-    def records(file_name: str):
-        return f'../records/{file_name}.json'
-
-
 @tree.command()
 async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message('pong')
+    await interaction.response.send_message(embed=discord.Embed(title='Pong!', description=''))
 
 
 @tree.command()
 @app_commands.describe(count='count')
 async def dice(interaction: discord.Interaction, count: int = 1):
-    nums = ['one', 'two', 'three', 'four', 'five', 'six']
     result: list[str] = []
     for _ in range(count):
-        result.append(f':{nums[randint(0, len(nums)-1)]}:')
+        result.append(f':{utility.Random.choice_item(['one', 'two', 'three', 'four', 'five', 'six'])}:')
     await interaction.response.send_message(str.join(' ', result))
 
 
@@ -59,8 +44,8 @@ async def nkodice(interaction: discord.Interaction):
 @tree.command()
 async def disconnect(interaction: discord.Interaction):
     if interaction.user.id != 283584931437871104:
-        return await interaction.response.send_message('権限がありません。', ephemeral=True)
-    await interaction.response.send_message('10秒後にオフラインになります。', ephemeral=True)
+        return await interaction.response.send_message(embed=Parameter.Embed.error('権限がありません。'), ephemeral=True)
+    await interaction.response.send_message(embed=Parameter.Embed.log('10秒後にオフラインになります。'), ephemeral=True)
     await asyncio.sleep(10)
     await asyncio.run(exit(0))
 
@@ -69,23 +54,26 @@ async def disconnect(interaction: discord.Interaction):
 @app_commands.describe(amount='金額', note='支払内容')
 async def cost_production(interaction: discord.Interaction, amount: int, note: Optional[str] = None):
     # 専用チャンネル外で使用
-    if not interaction.channel_id in channel_ids.values():
-        return await interaction.response.send_message(embed=utility.warn_embed(f'<#{channel_ids.get("redzone")}>で使用してください。'), ephemeral=True)
+    if not interaction.channel_id in Parameter.COST_CHANNEL_ID.values():
+        return await interaction.response.send_message(
+            embed=Parameter.Embed.warning(
+                f'{utility.Discord.mention(Parameter.COST_CHANNEL_ID.get("redzone"), utility.Discord.Mention.CHANNEL)}で使用してください。'),
+            ephemeral=True)
     # ファイルが存在しない
-    if not os.path.exists(file_path):
-        return await interaction.response.send_message(embed=utility.error_embed(f'ログファイルが存在しません。'), ephemeral=True)
+    if not os.path.exists(Parameter.LOG_FILE_PATH):
+        return await interaction.response.send_message(embed=Parameter.Embed.error(f'ログファイルが存在しません。'), ephemeral=True)
 
-    with open(file_path, 'r') as f:
+    with open(Parameter.LOG_FILE_PATH, 'r') as f:
         if (load_data := LogDict(json.load(f))) is None:
-            return await interaction.response.send_message('ファイルを読み込めませんでした。', ephemeral=True)
+            return await interaction.response.send_message(embed=Parameter.Embed.error('ファイルを読み込めませんでした。'), ephemeral=True)
 
-        pool, logs = load_data.get(LogKey.POOL), load_data.get(LogKey.LOGS)
+        pool = load_data.get(Parameter.Key.Log.POOL)
+        logs = load_data.get(Parameter.Key.Log.LOGS)
 
-        is_boss = interaction.user.id == user_ids.get('boss')
-
-        # ボス
-        if interaction.user.id == user_ids.get('boss'):
+        # ボスが使用した場合は直ぐ金額を反映
+        if is_boss := (interaction.user.id in Parameter.ADMIN_USER_ID.values()):
             pool += amount
+
         log = LogDataDict(
             id=len(logs),
             datetime=datetime.now().isoformat(),
@@ -93,28 +81,32 @@ async def cost_production(interaction: discord.Interaction, amount: int, note: O
             amount=amount,
             note=note,
             is_cancelled=False,
-            is_pending=not is_boss
+            is_pending=not is_boss,
+            message_id=interaction.message.id  # type: ignore
         )
         logs.append(log)
         load_data = LogDict(pool=pool, logs=logs)
-        with open(file_path, 'w') as ff:
+
+        with open(Parameter.LOG_FILE_PATH, 'w') as ff:
             json.dump(load_data, ff, indent=4)
             emb = discord.Embed(
-                title=utility.inline_code_block(f"#{log.get(LogDataKey.ID)}")+('' if is_boss else 'Pending...'),
+                title=utility.Discord.inline_code_block(f"#{log.get(Parameter.Key.LogData.ID)}") +
+                (utility.String.empty if is_boss else Parameter.Text.PENDING),
                 description='',
                 color=discord.Color.blue() if amount >= 0 else discord.Color.brand_red()
             )
-            emb.add_field(name='金額', value=utility.code_block(format(amount, ',')), inline=False)
-            if note != None:
-                emb.add_field(name='支払内容', value=utility.code_block(note), inline=False)
-            emb.add_field(name='チームプール', value=utility.code_block(format(pool, ',')), inline=False)
-            emb.set_footer(text='🔥REDZONE🔥')
+            emb.add_field(name=Parameter.Text.AMOUNT, value=utility.Discord.code_block(format(amount, ',')), inline=False)
+            if note is not None:
+                emb.add_field(name=Parameter.Text.NOTE, value=utility.Discord.code_block(note), inline=False)
+            emb.add_field(name=Parameter.Text.POOL, value=utility.Discord.code_block(format(pool, ',')), inline=False)
+            emb.set_footer(text=Parameter.Text.REDZONE)
+
             await interaction.response.send_message(embed=emb)
 
 
 def exists_log(logs: list[LogDataDict], log_id: int) -> bool:
     for log in logs:
-        if log.get(LogDataKey.ID) == log_id:
+        if log.get(Parameter.Key.LogData.ID) == log_id:
             return True
     return False
 
@@ -122,41 +114,59 @@ def exists_log(logs: list[LogDataDict], log_id: int) -> bool:
 @tree.command(name='cancel', description='取消')
 @app_commands.describe(id='log_id')
 async def cost_cancel(interaction: discord.Interaction, id: int):
-    if not interaction.channel_id in channel_ids.values():
-        return await interaction.response.send_message(embed=utility.warn_embed(f'<#{channel_ids.get("redzone")}>で使用してください。'), ephemeral=True)
+    if not interaction.channel_id in Parameter.COST_CHANNEL_ID.values():
+        return await interaction.response.send_message(
+            embed=Parameter.Embed.warning(f'{utility.Discord.mention(
+                Parameter.COST_CHANNEL_ID.get("redzone"), utility.Discord.Mention.CHANNEL)}で使用してください。'),
+            ephemeral=True
+        )
 
-    with open(file_path, 'r') as f:
+    with open(Parameter.LOG_FILE_PATH, 'r') as f:
         # ログファイルの読み込み失敗
-        if (latest_log_data := LogDict(json.load(f))) is None:
-            return await interaction.response.send_message('ファイルの読み込みに失敗しました。', ephemeral=True)
+        if ((latest_data := LogDict(json.load(f)))) is None:
+            return await interaction.response.send_message(
+                embed=Parameter.Embed.error('ファイルの読み込みに失敗しました。'),
+                ephemeral=True
+            )
 
-        logs = latest_log_data.get('logs')
+        logs = latest_data.get(Parameter.Key.Log.LOGS)
 
         # 無効なID(0未満・ログ数以上、存在しないID)が入力されたらリターン
         if len(logs) <= id < 0 or not exists_log(logs, id):
-            return await interaction.response.send_message(utility.error(f'{id} is invalid ID.'), ephemeral=True)
+            return await interaction.response.send_message(
+                embed=Parameter.Embed.error(f'{utility.Discord.inline_code_block(f"#{id}")} is invalid ID.'),
+                ephemeral=True
+            )
 
-        fixed_log_data = latest_log_data
+        fixed_data = latest_data
 
         for i in range(len(logs)):
-            if logs[i].get('id') != id:
+            if logs[i].get(Parameter.Key.LogData.ID) != id:
                 continue
-            if logs[i].get('is_cancelled'):
-                return await interaction.response.send_message('既にキャンセルされています。', ephemeral=True)
-            # 保留中でなければamountを引き、キャンセル
-            if not logs[i].get('is_pending'):
-                fixed_log_data['pool'] -= logs[i].get('amount')
-            fixed_log_data['logs'][i]['is_cancelled'] = True
+            if logs[i].get(Parameter.Key.LogData.IS_CANCELLED):
+                return await interaction.response.send_message(
+                    embed=Parameter.Embed.warning('既にキャンセルされています。'),
+                    ephemeral=True
+                )
 
-        with open(file_path, 'w') as f1:
-            json.dump(fixed_log_data, f1, indent=4)
+            # 保留中でなければamountを引き、キャンセル
+            if not logs[i].get(Parameter.Key.LogData.IS_PENDING):
+                fixed_data[Parameter.Key.Log.POOL] -= logs[i].get(Parameter.Key.LogData.AMOUNT)
+            fixed_data[Parameter.Key.Log.LOGS][i][Parameter.Key.LogData.IS_CANCELLED] = True
+
+        with open(Parameter.LOG_FILE_PATH, 'w') as f1:
+            json.dump(fixed_data, f1, indent=4)
         emb = discord.Embed(
-            title=f'{utility.inline_code_block(f"#{id}")} 取消',
+            title=f'{utility.Discord.inline_code_block(f"#{id}")} 取消',
             description='',
             colour=discord.Colour.light_gray()
         )
-        emb.add_field(name='チームプール', value=utility.code_block(format(fixed_log_data.get('pool'), ',')), inline=False)
-        emb.set_footer(text='🔥REDZONE🔥')
+        emb.add_field(
+            name=Parameter.Text.POOL,
+            value=utility.Discord.code_block(format(fixed_data.get(Parameter.Key.Log.POOL), ',')),
+            inline=False
+        )
+        emb.set_footer(text=Parameter.Text.REDZONE)
         await interaction.response.send_message(embed=emb)
 
 
---
+++ src/parameter.py
@@ -0,0 +1,77 @@
+﻿from enum import Enum
+from typing import Final, TypedDict, Optional
+import discord
+
+import utility
+
+
+class LogDataDict(TypedDict):
+    id: int
+    datetime: str
+    user_id: int
+    amount: int
+    note: Optional[str]
+    is_cancelled: bool
+    is_pending: bool
+    message_id: int
+
+
+class LogDict(TypedDict):
+    pool: int
+    logs: list[LogDataDict]
+
+
+class Parameter:
+    LOG_FILE_PATH = '../log/cost.json'
+    COST_CHANNEL_ID: Final[dict[str, int]] = {
+        'test': 1245694945323520031,
+        'redzone': 1222545209578229790
+    }
+    ADMIN_USER_ID: Final[dict[str, int]] = {
+        'boss': 421317690418790409,
+        'dev': 283584931437871104
+    }
+
+    class Key:
+        class LogData:
+            ID = 'id'
+            DATETIME = 'datetime'
+            USER_ID = 'user_id'
+            AMOUNT = 'amount'
+            NOTE = 'note'
+            IS_CANCELLED = 'is_cancelled'
+            IS_PENDING = 'is_pending'
+            MESSAGE_ID = 'message_id'
+
+        class Log:
+            POOL = 'pool'
+            LOGS = 'logs'
+
+    class Text:
+        REDZONE = '🔥REDZONE🔥'
+        AMOUNT = '金額'
+        NOTE = '支払内容'
+        POOL = 'チームプール'
+
+        PENDING = 'Pending...'
+        CANCEL = 'Cancel'
+        RECEIPT = 'Receipt'
+
+    class Embed:
+        @staticmethod
+        def error(description: str) -> discord.Embed:
+            embed = discord.Embed(title='Error', description=description, colour=discord.Colour.red())
+            embed.set_footer(text=Parameter.Text.REDZONE)
+            return embed
+
+        @staticmethod
+        def warning(description: str) -> discord.Embed:
+            embed = discord.Embed(title='Warning', description=description, colour=discord.Colour.pink())
+            embed.set_footer(text=Parameter.Text.REDZONE)
+            return embed
+
+        @staticmethod
+        def log(description: str) -> discord.Embed:
+            embed = discord.Embed(title='', description=description, colour=discord.Colour(0xdfdfdf))
+            embed.set_footer(text=Parameter.Text.REDZONE)
+            return embed
