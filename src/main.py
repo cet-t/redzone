390a72ddd68c9d6a83d42153be2a250d9bf76354
cet-t <cets021@gmail.com>
2024-07-29 23:35:38 +0900

    a

--- src/main.py
+++ src/main.py
@@ -81,7 +81,7 @@ async def cost_production(interaction: discord.Interaction, amount: int, note: O
             note=note,
             is_cancelled=False,
             is_pending=not is_boss,
-            message_id=interaction.id
+            message_id=interaction.message.id  # type: ignore
         )
         logs.append(log)
         load_data = LogDict(pool=pool, logs=logs)
